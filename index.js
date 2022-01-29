48e28cf68b02f7fcbdb015c4654b39ba86aca377
unknown <ncis@hotmail.de>
2022-01-29 17:34:51 +0100

    Navigation Ruotes

---
+++ index.js
@@ -0,0 +1,11 @@
+let home = "HOME"
+let team = "TEAM"
+let impressum = "IMPRESSUM"
+let picture = "BILDER"
+let contact = "KONTAKT"
+
+document.getElementById('Home').innerHTML = home;
+document.getElementById('Team').innerHTML = team;
+document.getElementById('Impressum').innerHTML= impressum;
+document.getElementById('Bilder').innerHTML = picture;
+document.getElementById('Kontakt').innerHTML = contact;
