6551ce22d54924c4cc6fdb94237f119236cee40f
unknown <ncis@hotmail.de>
2022-01-30 15:44:12 +0100

    Landingpage

--- index.js
+++ index.js
@@ -4,8 +4,16 @@ let impressum = "IMPRESSUM"
 let picture = "BILDER"
 let contact = "KONTAKT"
 
+let Comradeship = "Kameradschaft";
+let aboutUs = "ÜberUns";
+let Application = "Bewerbung";
+
 document.getElementById('Home').innerHTML = home;
 document.getElementById('Team').innerHTML = team;
 document.getElementById('Impressum').innerHTML= impressum;
 document.getElementById('Bilder').innerHTML = picture;
 document.getElementById('Kontakt').innerHTML = contact;
+
+document.getElementById('Kameradschaft').innerHTML = Comradeship
+document.getElementById('ÜberUns').innerHTML = aboutUs;
+document.getElementById('Bewerbung').innerHTML = Application;
