f36c2243a276e3c43dd4d0749b0e3a7255981359
unknown <ncis@hotmail.de>
2022-01-30 16:46:44 +0100

    Homepage Fixed

--- index.js
+++ index.js
@@ -1,19 +1,37 @@
+// For Text Changes Navigation
 let home = "HOME"
 let team = "TEAM"
 let impressum = "IMPRESSUM"
 let picture = "BILDER"
 let contact = "KONTAKT"
 
+// For Text Changes Homepage
 let Comradeship = "Kameradschaft";
+let ComradeshipText = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae quas magnam ullam, fugiat quam nihil harum officiis, id fugit pariatur tempore obcaecati et veniam eaque ducimus! Amet, quisquam iure. Voluptatibus?";
 let aboutUs = "ÜberUns";
+let aboutUsText = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae quas magnam ullam, fugiat quam nihil harum officiis, id fugit pariatur tempore obcaecati et veniam eaque ducimus! Amet, quisquam iure. Voluptatibus?";
 let Application = "Bewerbung";
+let ApplicationText = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae quas magnam ullam, fugiat quam nihil harum officiis, id fugit pariatur tempore obcaecati et veniam eaque ducimus! Amet, quisquam iure. Voluptatibus?";
 
+
+// Dont Change anything Here //
+
+// Navigation Module
 document.getElementById('Home').innerHTML = home;
 document.getElementById('Team').innerHTML = team;
-document.getElementById('Impressum').innerHTML= impressum;
+document.getElementById('Impressum').innerHTML = impressum;
 document.getElementById('Bilder').innerHTML = picture;
 document.getElementById('Kontakt').innerHTML = contact;
 
-document.getElementById('Kameradschaft').innerHTML = Comradeship
+
+// Comradeship Module
+document.getElementById('Kameradschaft').innerHTML = Comradeship;
+document.getElementById('KText').innerHTML = ComradeshipText;
+
+// About Us Module
 document.getElementById('ÜberUns').innerHTML = aboutUs;
+document.getElementById('AUText').innerHTML = aboutUsText;
+
+// Application Module
 document.getElementById('Bewerbung').innerHTML = Application;
+document.getElementById('BWText').innerHTML = ApplicationText;
